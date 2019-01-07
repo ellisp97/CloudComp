f2a78d5b4f21519e458799df93689e0c821f62c8
ellisp97 <ellis97.ep@gmail.com>
2019-01-07 21:38:46 +0000

    recipe config

---
+++ js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_2o8G2GU07', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '414jagrmik5ivvtaf1dg62d688', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
+
---
+++ js/main.js
@@ -0,0 +1,3 @@
+function goRecipePage() {
+    location.href = "http://cloud-coursework-manan-ellis.s3-website.eu-west-2.amazonaws.com/submit.html";
+}
